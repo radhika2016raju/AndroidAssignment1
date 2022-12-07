ca73154c41002fe799842ccfbf794bca41b0ac2f
radhika2016raju <103647710+radhika2016raju@users.noreply.github.com>
2022-12-07 13:54:38 +0530

    Commit

---
+++ app/src/main/java/com/example/myassignment1/PrefConstants.kt
@@ -0,0 +1,9 @@
+package com.example.myassignment1
+
+object PrefConstants {
+    const val PREF_NAME = "SharedPreferences"
+    const val IS_LOGIN = "is_login"
+    const val PRIVATE_MODE = 0
+
+
+}
